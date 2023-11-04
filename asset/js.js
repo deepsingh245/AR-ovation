f69e3441c063f7ceb5ccb65858936e0685e5101e
Deep Singh <deepsingh25244@gmail.com>
2023-11-04 15:34:30 +0530

    updated

---
+++ asset/js.js
@@ -0,0 +1,74 @@
+
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    const myimage=document.getElementById('image')
+    const left=document.getElementById('left-but')
+    const right=document.getElementById('right-but')
+    const heartBut=document.getElementById('heart-but')
+    const model=document.getElementById('heart')
+
+    var num=['heart.jpg', 'crazyvault.jpeg','try.png'];
+    var lastIndex = 0;
+    var rotation = 90;
+    left.addEventListener('click', function(evt){
+      if (lastIndex<1){
+        lastIndex=3
+      }
+      lastIndex = (lastIndex - 1) % num.length;
+      myimage.setAttribute('src',`./`+`${num[lastIndex]} `)
+    })
+
+    right.addEventListener('click', function(evt){
+      lastIndex = (lastIndex + 1) % num.length;
+      myimage.setAttribute('src',`./`+`${num[lastIndex]} `)
+    })
+
+    heartBut.addEventListener('click', function(evt){
+    rotation = rotation+30;
+    model.setAttribute('rotation', `0 `+`0`+` 0`)
+    console.log(`0 `+`${rotation}`+` 0`)
+    })
+
+  }});
+
+
+
+
+
+  AFRAME.registerComponent('drag-rotate-component',{
+    schema : { speed : {default:1}},
+    init : function(){
+      this.ifMouseDown = false;
+      this.x_cord = 0;
+      this.y_cord = 0;
+      document.addEventListener('mousedown',this.OnDocumentMouseDown.bind(this));
+      document.addEventListener('mouseup',this.OnDocumentMouseUp.bind(this));
+      document.addEventListener('mousemove',this.OnDocumentMouseMove.bind(this));
+    },
+    OnDocumentMouseDown : function(event){
+      this.ifMouseDown = true;
+      this.x_cord = event.clientX;
+      this.y_cord = event.clientY;
+    },
+    OnDocumentMouseUp : function(){
+      this.ifMouseDown = false;
+    },
+    OnDocumentMouseMove : function(event)
+    {
+      if(this.ifMouseDown)
+      {
+        console.log('halfdone',)
+        var temp_x = event.clientX-this.x_cord;
+        var temp_y = event.clientY-this.y_cord;
+        this.el.object3D.rotateY(temp_y*this.data.speed/100);
+        this.el.object3D.rotateX(temp_x*this.data.speed/100);
+    
+        this.x_cord = event.clientX;
+        this.y_cord = event.clientY;
+      }
+    }
+  });
+
+
+
+  
